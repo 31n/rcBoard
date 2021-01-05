e10d7e85fb28cbfabb858992818b379292acc6d1
31n <41046238+31n@users.noreply.github.com>
2021-01-05 14:12:57 +0900

    Initial Commit

---
+++ Shared/rcBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  rcBoardApp.swift
+//  Shared
+//
+//  Created by Eizo Ishikawa on 2021/01/05.
+//
+
+import SwiftUI
+
+@main
+struct rcBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
