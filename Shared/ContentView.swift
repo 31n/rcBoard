0e3faf76c19168ded160a4983dc7b5bed91175ef
31n <41046238+31n@users.noreply.github.com>
2021-01-05 15:15:15 +0900

    - Connect to server-side, and change the file via PHP request.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var rcbServerIP = "172.20.10.3"
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Control for RinachanBoard")
+            .font(.title)
+        HStack{
+            Spacer(minLength: 10)
+            TextField("IP address", text: $rcbServerIP)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Spacer(minLength: 10)
+            Button(action: {setIP(address: rcbServerIP)}) {
+                Text("OK")
+            }
+            Spacer(minLength: 10)
+        }
+        Button(action: {sendServer(boardID: 1)}) {
+            Text("Button 1")
+        }
+        Button(action: {sendServer(boardID: 2)}) {
+            Text("Button 2")
+        }
+    }
+    
+    func setIP(address: String){
+        
+        print(address)
+    }
+    
+    func sendServer(boardID: Int){
+        let link = URL(string: "http://" + rcbServerIP + "/project/rcb/main.php?boardid=" + String(boardID))!
+        let request = URLRequest(url: link)
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if error == nil, let data = data, let response = response as? HTTPURLResponse {
+                // HTTPヘッダの取得
+                print("Content-Type: \(response.allHeaderFields["Content-Type"] ?? "")")
+                // HTTPステータスコード
+                print("statusCode: \(response.statusCode)")
+                print(String(data: data, encoding: String.Encoding.utf8) ?? "")
+            }
+        }.resume()
+        
     }
 }
 
