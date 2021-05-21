4439eb0c82fb2bd6e87e913a7eecaef563e8fddb
31n <41046238+31n@users.noreply.github.com>
2021-05-22 07:04:58 +0900

    Improved plural face process

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,15 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var rcbServerIP = "172.20.10.3"
+    @State var rcbServerIP = "18.183.171.202"
     let lYellow = Color(red: 255/255, green: 224/255, blue: 94/255)
     let dYellow = Color(red: 213/255, green: 157/255, blue: 48/255)
     let eBlue = Color(red: 165/255, green: 221/255, blue: 222/255)
     let eBlack = Color(red: 82/255, green: 79/255, blue: 63/255)
-    let face = ["にっこりん", "むんっ", "悲しい"]
+    let face = ["にっこりん", "むんっ", "悲しい", "嬉しい", "ウインク", "驚き"]
+    //let faceNum = 4
     var body: some View {
+        Spacer(minLength: 20)
         Text("Control for RinachanBoard")
             .font(.title)
+        Spacer(minLength: 20)
         HStack{
             Spacer(minLength: 10)
             TextField("IP address", text: $rcbServerIP)
@@ -27,32 +30,57 @@ struct ContentView: View {
             }
             Spacer(minLength: 10)
         }
+        Spacer(minLength: 20)
+        let HLen = Int(floor(Double((face.count)/4)))
         VStack{
-            HStack{
-                ForEach(0..<3) { i in
-                    Spacer()
-                    Button(action: {sendServer(boardID: i)}, label: {
-                        ZStack{
-                            Text(face[i])
-                                .accentColor(eBlack)
-                            Circle()
-                                .stroke(dYellow, lineWidth: 2)
-                                .frame(width: 78, height: 78)
+            VStack{
+                ForEach(0..<HLen) { j in
+                    HStack{
+                        ForEach((j*4)..<(j*4+4)) { i in
+                            Spacer()
+                            Button(action: {sendServer(boardID: i)}, label: {
+                                ZStack{
+                                    Text(face[i])
+                                        .accentColor(eBlack)
+                                    Circle()
+                                        .stroke(dYellow, lineWidth: 2)
+                                        .frame(width: 78, height: 78)
+                                }
+                            })
+                            Spacer()
                         }
-                    })
-                    Spacer()
+                    }
+
                 }
+                HStack{
+                    ForEach(4*HLen..<face.count) { i in
+                        Spacer()
+                        Button(action: {sendServer(boardID: i)}, label: {
+                            ZStack{
+                                Text(face[i])
+                                    .accentColor(eBlack)
+                                Circle()
+                                    .stroke(dYellow, lineWidth: 2)
+                                    .frame(width: 78, height: 78)
+                            }
+                        })
+                        Spacer()
+                    }
+                }
+                
             }
         }
+        Spacer()
     }
     
     func setIP(address: String){
-        
+        print(Int(floor(Double(face.count)/4)))
         print(address)
     }
     
+    
     func sendServer(boardID: Int){
-        let link = URL(string: "http://" + rcbServerIP + "/project/rcb/main.php?boardid=" + String(boardID))!
+        let link = URL(string: "http://" + rcbServerIP + "/RCB/main.php?boardid=" + String(boardID))!
         let request = URLRequest(url: link)
         let session = URLSession.shared
         session.dataTask(with: request) { (data, response, error) in
